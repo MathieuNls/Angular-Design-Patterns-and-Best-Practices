d7c8aaff1a8fc191e4d986344ef1955c8e641571
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-07-06 23:05:05 -0400

    add directive

--- chap2/src/app/floyd/floyd.component.ts
+++ chap2/src/app/floyd/floyd.component.ts
@@ -1,30 +1,41 @@
 import { Component } from '@angular/core';
 import { TriangleService } from '../triangle.service';
-
+import { AngularPre } from './../angular-pre.directive';
 @Component({
   selector: 'floyd',
   template: 
   	`<p>
+  	  <input #checkbox type="checkbox" value="even">Even?<br>
 	  <input #rows type="text" name="rows">
-	  <button (click)="onClick(rows.value)">CLICK</button>
+	  <button (click)="onClick(rows.value, checkbox.checked)">CLICK</button>
 	</p>
-	<pre>
+	<pre AngularPre [highlightColor]="color">
+	
 		{{floydString}}
 	</pre>
 	`,
-	providers:   [TriangleService]
+	providers:   [TriangleService],
+	directives: [AngularPre]
 })
 export class FloydComponent {
 
   private floydString:string = "";
+  private color:"yellow" | "red";
 
   constructor(private triangleService:TriangleService) {
 
   }
 
-  onClick(rows:number){
+  onClick(rows:number, even:boolean){
 
-  	this.floydString = this.triangleService.evenFloydTriangle(rows);
+  	if(even){
+  		this.floydString = this.triangleService.evenFloydTriangle(rows);
+  		this.color = "red";
+  	}else{
+  		this.floydString = this.triangleService.floydTriangle(rows);
+  		this.color = "yellow";
+  	}
+
   }
 
 }
--- chap2/src/main.ts
+++ chap2/src/main.ts
@@ -1,5 +1,6 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
+import { environment } from './app/';
 import { FloydComponent } from './app/floyd/';
 import { TriangleService } from './app/triangle.service'
 
